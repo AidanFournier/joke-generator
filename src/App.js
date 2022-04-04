b106fe42f20a960bd021241c16c617b36fd95a51
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-04-04 23:39:01 +0900

    Passes non-string props into joke component

--- src/App.js
+++ src/App.js
@@ -7,22 +7,30 @@ export default function App() {
             <Joke 
               punchline="It’s hard to explain puns to kleptomaniacs because 
               they always take things literally."
+              isPun={true}
+              upvotes={10}
+              downvotes={2}
+              comments={[{author: "", body: "", title: ""}]}
             />
             <Joke 
               setup="Parallel lines have so much in common."
               punchline="It's a shame they'll never meet."
+              isPun={false}
             />
             <Joke 
               setup="Someeone stole my mood ring..."
               punchline="I don't know how to feel about that."
+              isPun={false}
             />
             <Joke 
               setup="I broke my finger last week."
               punchline="On the other hand, I'm okay."
+              isPun={false}
             />
             <Joke 
-              setup="Someone stole my Microsoft Office annd they're gonna pay."
+              setup="Someone stole my Microsoft Office and they're gonna pay."
               punchline="You have my word."
+              isPun={true}
             />
         </div>
     )
